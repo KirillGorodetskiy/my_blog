300b6ff1ae0e2a99c6300763ed0f94c2989ff901
Kirill <kirill.gorodetskiy@gmail.com>
2025-04-13 14:09:08 +0000

    Changed settings for server

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', '212.227.250.38', 'localhost', 'gkablog.com']
 
 
 # Application definition
