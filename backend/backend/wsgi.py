9e78a27a53b6a8092318c20f4f9a07e00a622d9e
Kirill G <kalipso174174@gmail.com>
2025-10-31 14:54:07 +0000

    WSGI fixed

--- backend/backend/wsgi.py
+++ backend/backend/wsgi.py
@@ -1,17 +1,7 @@
 import os
-import sys
 from django.core.wsgi import get_wsgi_application
 
-# --- AUTO-FIND PROJECT ROOT ---
-project_root = os.path.dirname(
-    os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    )
 
-# Add to Python path
-if project_root not in sys.path:
-    sys.path.insert(0, project_root)
-
-# Now 'backend' refers to /var/www/my_blog/backend/
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.backend.settings')
 
 application = get_wsgi_application()
